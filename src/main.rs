40f1a644d6dd5c090436b5d654d403e094ffff2c
salad <fmoenne-@student.le-101.fr>
2021-03-03 21:43:26 +0100

    sarace le rust

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,21 @@ use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use std::io::{Write, stdout, stdin};
 
+fn print_char(c: char, xpos: &mut i32, ypos: &mut i32) {
+    print!("{}", c);
+    *xpos = *xpos + 1;
+    print!("{}", xpos);
+}
+
 fn edit(stdin: std::io::Stdin, stdout: std::io::Stdout) {
     let mut out = stdout.into_raw_mode().unwrap();
+    let mut xpos: i32 = 1;
+    let mut ypos: i32 = 1;
 
     for c in stdin.keys() {
         match c.unwrap() {
             Key::Char('q') => break,
-            Key::Char(c) => print!("{}", c),
+            Key::Char(c) => print_char(c, &mut xpos, &mut ypos),
             Key::Alt(c) => print!("^{}", c),
             Key::Ctrl(c) => print!("*{}", c),
             Key::Esc => print!("ESC"),
@@ -32,7 +40,7 @@ fn main() {
     let mut stdout = stdout();
 
     write!(stdout,
-           "{}{}q to exit. Welcome to JUTE, a very nice text editor{}",
+           "{}{}Welcome to JUTE, a very nice text editor. press q to exit. {}",
            termion::clear::All,
            termion::cursor::Goto(1, 1),
            termion::cursor::Hide)
