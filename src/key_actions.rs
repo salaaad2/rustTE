83d20220bd397fa63d1d2c7966b656f9b7b0012b
salad <fmoenne-@student.le-101.fr>
2021-03-04 22:38:55 +0100

    pretty good huh

--- src/key_actions.rs
+++ src/key_actions.rs
@@ -1,7 +1,22 @@
+use termion::terminal_size;
+
 use crate::SPos;
 
 pub fn print_char(c: char, pos: &mut SPos) {
+    let tuple = terminal_size().unwrap();
     print!("{}", c);
     pos.xpos = pos.xpos + 1;
-    print!("{}", termion::cursor::Goto(pos.xpos, pos.ypos));
+    //println!("Size is {}", tuple.0);
+    if pos.xpos == tuple.0
+    {
+        pos.ypos = pos.ypos + 1;
+        pos.xpos = 1;
+        print!("{}", termion::cursor::Goto(pos.xpos, pos.ypos));
+    }
+    if c == '\n'
+    {
+        pos.ypos = pos.ypos + 1;
+        pos.xpos = 1;
+        print!("{}", termion::cursor::Goto(pos.xpos, pos.ypos));
+    }
 }
